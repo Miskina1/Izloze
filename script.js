3646a5402b39c9ff277a1ad32c3515d41ce569f4
Miskina1 <montamishkina1@gmail.com>
2022-02-07 15:11:29 +0200

    Finals

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const vardi=['Ieva Cīrule', 'Laura Ziede','Jānis Ozols', 'Normunds Kļava'];
 const balvas= ['Dators', 'Veļasmašīna', 'Viedtālrunis', 'Automašīna','Grāmata'];
 const naudaKopa=1000000;// kopēja summa izlozei
-let uzvaretajuSkaits=5;
+let uzvaretajuSkaits=5; 
 let rand= Math.random()*5;
 let rindas = document.querySelector('.rindas');
 rindas.innerHTML = '';
@@ -10,9 +10,11 @@ for (let i=0; i< uzvaretajuSkaits; i++){
     rand=Math.floor(rand); // noapaļo skaitļus uz leju
 
     let uzvaretajs = vardi[rand];
+    let davanas = balvas[rand];
     rindas.innerHTML +=`
     <tr>
     <td> ${i+1}</td>
     <td> ${uzvaretajs}</td>
+    <td>${davanas}</td> 
     </tr>`
 }
